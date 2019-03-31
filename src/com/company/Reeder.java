d2b18755b684ef252d7c411c4e23c4f14c731ead
Dima Chuyko <cdi880@gmail.com>
2019-03-31 23:14:05 +0300

    Закончил клас Vinner который определяет победителя.

--- src/com/company/Reeder.java
+++ src/com/company/Reeder.java
@@ -7,8 +7,8 @@ class Reeder {
    // метод readeKey() читает введённые числа из клавиатуры  возвращает число от 1 до 9
       int readerKey(){
          Scanner keyboard = new Scanner(System.in);
-         int myint = keyboard.nextInt();
-         return myint;
+        return keyboard.nextInt();
+
      }
 
 }
--- src/com/company/Vinner.java
+++ src/com/company/Vinner.java
@@ -1,18 +1,20 @@
 package com.company;
-
+// Класс Vinner определяет победителя с помощью метода vinDetect
 public class Vinner {
     void vinDetect(int[][]pol){
 for (int a = 0; a < 11; a+=10) {
            if
-           ((pol[0][0] == a && pol[0][1] == a && pol[0][2] == a) &&
-                   (pol[1][0] == a && pol[1][1] == a && pol[1][2] == a) &&
-                   (pol[2][0] == a && pol[2][1] == a && pol[2][2] == a) &&
-                   (pol[0][0] == a && pol[1][0] == a && pol[2][0] == a) &&
-                   (pol[0][1] == a && pol[1][1] == a && pol[2][1] == a) &&
-                   (pol[0][2] == a && pol[1][2] == a && pol[2][2] == a) &&
-                   (pol[0][0] == a && pol[1][1] == a && pol[2][2] == a) &&
+           ((pol[0][0] == a && pol[0][1] == a && pol[0][2] == a) ||
+
+                   (pol[1][0] == a && pol[1][1] == a && pol[1][2] == a) ||
+                   (pol[2][0] == a && pol[2][1] == a && pol[2][2] == a) ||
+                   (pol[0][0] == a && pol[1][0] == a && pol[2][0] == a) ||
+                   (pol[0][1] == a && pol[1][1] == a && pol[2][1] == a) ||
+                   (pol[0][2] == a && pol[1][2] == a && pol[2][2] == a) ||
+                   (pol[0][0] == a && pol[1][1] == a && pol[2][2] == a) ||
                    (pol[0][2] == a && pol[1][1] == a && pol[2][0] == a)){
                if (a==0){
+                   System.out.println();
                System.out.println("ВЫИГРАЛИ НОЛИКИ");}
                else System.out.println("ВЫИГРАЛИ КРЕСТИКИ");
            }
